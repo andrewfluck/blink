825332f80a381543e67e6eaad52c79be18ff03bb
Anas Karbila <anaskarbila@aospa.co>
2018-09-12 17:19:00 +0200

    BlinkView: small cleanup & detect item actions

--- app/src/main/java/com/andrewfluck/blink/widget/BlinkView.java
+++ app/src/main/java/com/andrewfluck/blink/widget/BlinkView.java
@@ -7,13 +7,10 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.drawable.Drawable;
+import android.graphics.Rect;
 import android.os.Vibrator;
-import android.provider.Settings;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.OvershootInterpolator;
@@ -23,9 +20,7 @@ import android.widget.LinearLayout;
 import com.andrewfluck.blink.R;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class BlinkView extends LinearLayout {
     private final String TAG = BlinkView.class.getSimpleName();
@@ -35,31 +30,19 @@ public class BlinkView extends LinearLayout {
     private int mViewWidth;
     private int mViewHeight;
 
-    private int mBlinkColorResId;
-    private Drawable mBackDrawable;
-    private Drawable mRotateDrawable;
-
-    private long lastDown = 0;
-    private long lastUp = 0;
-
-    private float mBlinkWidth;
     private float mBlinkHeight;
+    private float mBlinkWidth;
     private float mBlinkHalfWidth;
     private float mBlinkHalfHeight;
 
-    private int mQuickstepHomeWidth;
     private int mQuickstepHomeHeight;
-    private float mAssistantDotRadius;
-    private float mAssistantDotGap;
 
     private float mOffsetX;
     private float mOffsetY;
 
-    private Resources mResources;
-    private Context mContext;
     private Paint mBlinkBackground;
-    private Paint mQuickstepHomeFill;
-    private Vibrator mVibrator;
+
+    private final Path mBlinkPath = new Path();
 
     private enum Actions {
         BACK,
@@ -70,13 +53,11 @@ public class BlinkView extends LinearLayout {
     private final List<Actions> mActions = new ArrayList<>();
 
     public BlinkView(Context context) {
-        super(context);
-        init(context);
+        this(context, null);
     }
 
     public BlinkView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init(context);
+        this(context, attrs, 0);
     }
 
     public BlinkView(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -89,58 +70,53 @@ public class BlinkView extends LinearLayout {
 
         mOpen = false;
 
-        mContext = context;
-        mResources = mContext.getResources();
-        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        Resources res = context.getResources();
 
         /* Blink blob dimensions */
-        mBlinkWidth = mResources.getDimension(R.dimen.blink_width);
-        mBlinkHeight = mResources.getDimension(R.dimen.blink_height);
+        mBlinkWidth = res.getDimension(R.dimen.blink_width);
+        mBlinkHeight = res.getDimension(R.dimen.blink_height);
         mBlinkHalfWidth = mBlinkWidth / 2;
         mBlinkHalfHeight = mBlinkHeight / 2;
-
-        mBlinkColorResId = mResources.getColor(R.color.blink_blob_bg_color);
-
-        mBackDrawable = mResources.getDrawable(R.drawable.ic_sysbar_back_button);
-        mRotateDrawable = mResources.getDrawable(R.drawable.ic_sysbar_rotate_button);
 
         /* Blink blob offsets */
         mOffsetX = 0;
         mOffsetY = mBlinkHeight;
 
         /* Quickstep home dimensions */
-        mQuickstepHomeWidth = mResources.getDimensionPixelSize(R.dimen.quickstep_home_width);
-        mQuickstepHomeHeight = mResources.getDimensionPixelSize(R.dimen.quickstep_home_height);
-        mAssistantDotRadius = mResources.getDimension(R.dimen.assistant_dot_radius);
-        mAssistantDotGap = mResources.getDimension(R.dimen.assistant_dot_gap);
+        mQuickstepHomeHeight = res.getDimensionPixelSize(R.dimen.quickstep_home_height);
 
         /* Blink blob paint */
         mBlinkBackground = new Paint();
         mBlinkBackground.setAntiAlias(true);
         mBlinkBackground.setStyle(Paint.Style.FILL);
-        mBlinkBackground.setColor(mBlinkColorResId);
+        mBlinkBackground.setColor(res.getColor(R.color.blink_blob_bg_color));
 
         /* Quickstep home paint */
-        mQuickstepHomeFill = new Paint();
-        mQuickstepHomeFill.setAntiAlias(true);
-        mQuickstepHomeFill.setStyle(Paint.Style.FILL);
-        mQuickstepHomeFill.setColor(Color.BLACK);
-        mQuickstepHomeFill.setStrokeWidth(mQuickstepHomeHeight);
-        mQuickstepHomeFill.setStrokeCap(Paint.Cap.ROUND);
-        mQuickstepHomeFill.setAlpha(98);
+        final Paint quickstepHomeFill = new Paint();
+        quickstepHomeFill.setAntiAlias(true);
+        quickstepHomeFill.setStyle(Paint.Style.FILL);
+        quickstepHomeFill.setColor(Color.BLACK);
+        quickstepHomeFill.setStrokeWidth(res.getDimensionPixelSize(R.dimen.quickstep_home_height));
+        quickstepHomeFill.setStrokeCap(Paint.Cap.ROUND);
+        quickstepHomeFill.setAlpha(98);
     }
 
     @Override
     public void onAttachedToWindow() {
         super.onAttachedToWindow();
-        setPadding((int)  mBlinkHalfWidth, 0, (int) mBlinkHalfWidth, 0);
 
+        setPadding((int) mBlinkHalfWidth, 0, (int) mBlinkHalfWidth, 0);
+
+        layoutItems();
+    }
+
+    private void layoutItems() {
         mActions.add(Actions.BACK);
         mActions.add(Actions.HOME);
         mActions.add(Actions.RECENTS);
 
         int N = mActions.size();
-        float weightSum = 1f;// / N;
+        float weightSum = 1f;
 
         for (int i = 0; i < N; i++) {
             Actions action = mActions.get(i);
@@ -165,7 +141,6 @@ public class BlinkView extends LinearLayout {
                     LayoutParams.WRAP_CONTENT,
                     LayoutParams.WRAP_CONTENT
             );
-            android.util.Log.e("ANAS","weightSum="+weightSum);
             lp.weight = weightSum;
             lp.gravity = Gravity.CENTER_VERTICAL;
             addView(imageView, lp);
@@ -180,45 +155,6 @@ public class BlinkView extends LinearLayout {
         mViewHeight = h;
     }
 
-    private void drawBlinkBlob(Canvas canvas, float offsetX, float offsetY) {
-//        final float blinkOffsetX = offsetX * 0.3f;
-//        final float blinkOffsetY = offsetY * 1.0f;
-//        final float qsOffsetX = offsetX * 0.25f;
-//        final float qsOffsetY = offsetY * 0.3f;
-//        final int threshold = (int) Math.abs(qsOffsetX / (mBlinkHalfWidth / 4) * 100);
-//
-//        final Path mBlinkPath = new Path();
-//
-//        float fourthWidth = mBlinkWidth / 4;
-//        mBlinkPath.moveTo(-mBlinkHalfWidth, mBlinkHeight);
-//        mBlinkPath.cubicTo(-fourthWidth + blinkOffsetX, mBlinkHeight, -fourthWidth + blinkOffsetX, blinkOffsetY, blinkOffsetX, blinkOffsetY);
-//        mBlinkPath.cubicTo(fourthWidth + blinkOffsetX, blinkOffsetY, fourthWidth + blinkOffsetX, mBlinkHeight, mBlinkHalfWidth, mBlinkHeight);
-//        mBlinkPath.close();
-//
-//        int alpha = (int) clamp(100 - threshold, 0, 100);
-//        int N = getChildCount();
-//        for (int i = 0; i < N; i++) {
-//            View child = getChildAt(i);
-//            child.setAlpha(alpha);
-//        }
-//
-//        canvas.clipPath(mBlinkPath);
-//        canvas.drawPath(mBlinkPath, mBlinkBackground);
-
-//        for (int i = 0; mBlinkAssistantColorsResId.length > i; i++) {
-//            Paint dotPaint = new Paint();
-//            dotPaint.setAntiAlias(true);
-//            dotPaint.setStyle(Paint.Style.FILL);
-//            dotPaint.setColor(mBlinkAssistantColorsResId[i]);
-//
-//            canvas.drawCircle((i - (mBlinkAssistantColorsResId.length / 2.666666666f)) * 36, mBlinkHalfHeight + qsOffsetY, mAssistantDotRadius, dotPaint);
-//        }
-    }
-
-    private static float clamp(float val, float min, float max) {
-        return Math.max(min, Math.min(max, val));
-    }
-
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -228,55 +164,51 @@ public class BlinkView extends LinearLayout {
             return;
         }
 
-
+        mBlinkPath.reset();
 
         final float blinkOffsetX = mOffsetX * 0.3f;
         final float blinkOffsetY = mOffsetY * 1.0f;
-        final float qsOffsetX = mOffsetX * 0.25f;
-        final float qsOffsetY = mOffsetY * 0.3f;
-        final int threshold = (int) Math.abs(qsOffsetX / (mBlinkHalfWidth / 4) * 100);
-
-        final Path mBlinkPath = new Path();
 
         float fourthWidth = mBlinkWidth / 4;
         mBlinkPath.moveTo(-mBlinkHalfWidth, mBlinkHeight);
-        mBlinkPath.cubicTo(-fourthWidth + blinkOffsetX, mBlinkHeight, -fourthWidth + blinkOffsetX, blinkOffsetY, blinkOffsetX, blinkOffsetY);
-        mBlinkPath.cubicTo(fourthWidth + blinkOffsetX, blinkOffsetY, fourthWidth + blinkOffsetX, mBlinkHeight, mBlinkHalfWidth, mBlinkHeight);
+        mBlinkPath.cubicTo(-fourthWidth + blinkOffsetX, mBlinkHeight, -fourthWidth + blinkOffsetX,
+                blinkOffsetY, blinkOffsetX, blinkOffsetY);
+        mBlinkPath.cubicTo(fourthWidth + blinkOffsetX, blinkOffsetY, fourthWidth + blinkOffsetX,
+                mBlinkHeight, mBlinkHalfWidth, mBlinkHeight);
         mBlinkPath.close();
-
-        int alpha = (int) clamp(100 - threshold, 0, 100);
-        int N = getChildCount();
-        for (int i = 0; i < N; i++) {
-            View child = getChildAt(i);
-            child.setAlpha(alpha);
-        }
-
-
 
         canvas.translate(mViewWidth / 2, 0);
         canvas.clipPath(mBlinkPath);
         canvas.drawPath(mBlinkPath, mBlinkBackground);
         canvas.translate(-(mViewWidth / 2), 0);
-
     }
 
+    @Override
+    @SuppressWarnings("ClickableViewAccessibility")
     public boolean onTouchEvent(MotionEvent event) {
-
         switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN: {
-                lastDown = System.currentTimeMillis();
-//                Log.d(TAG, Long.toString(lastDown));
-//                Log.d(TAG, "Down");
-                break;
-            }
             case MotionEvent.ACTION_UP: {
-                lastUp = System.currentTimeMillis();
-//                Log.d(TAG, Long.toString(lastUp));
-//                Log.d(TAG, "Up");
-                Log.d(TAG, Long.toString(lastUp - lastDown));
-                if ((lastUp - lastDown) > 250 && event.getY() <= 0) {
-//                    mVibrator.vibrate(250);
-//                    mContext.startActivity(new Intent(Intent.ACTION_VOICE_COMMAND).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                if (mOpen) {
+                    int x = (int) event.getX();
+
+                    int N = getChildCount();
+                    for (int i = 0; i < N; i++) {
+                        View v = getChildAt(i);
+                        Rect clickZone = new Rect();
+                        v.getGlobalVisibleRect(clickZone);
+
+                        if (clickZone.left < clickZone.right && clickZone.top < clickZone.bottom) {
+                            if (x >= clickZone.left && x < clickZone.right) {
+                                switch ((Actions) v.getTag()) {
+                                    case BACK:
+                                    case HOME:
+                                    case RECENTS:
+                                }
+                                // Action triggered
+                                //android.util.Log.e("ANAS", "tag="+v.getTag());
+                            }
+                        }
+                    }
                 }
 
                 animateClose();
@@ -289,7 +221,6 @@ public class BlinkView extends LinearLayout {
 
                 float newOffsetX = event.getX() - (mViewWidth / 2);
                 float newOffsetY = event.getY();
-
 
                 if (newOffsetY <= (mViewHeight - mBlinkHeight)) {
                     mOffsetY = mViewHeight - mBlinkHeight;
@@ -330,11 +261,5 @@ public class BlinkView extends LinearLayout {
 
         resetX.start();
         resetY.start();
-    }
-
-    private boolean assistantAvailable(Context context) {
-        final String setting = Settings.Secure.getString(context.getContentResolver(), "assistant");
-
-        return setting != null;
     }
 }
