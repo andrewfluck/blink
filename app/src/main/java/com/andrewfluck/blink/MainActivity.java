8616724cb517eb1b8312c991e3d03df5c9404657
Hernán Castañón Álvarez <hernanderes34@gmail.com>
2019-01-09 04:55:13 +0100

    Blink: Make the app go fullscreen mode, navbar only.
    Navigation bar will show up for 2-3 seconds but then hide, so you get a full experience of Blink.
    
    * TODO: Make it permanent, which I think can´t be done, since it would be a security flaw.

--- app/src/main/java/com/andrewfluck/blink/MainActivity.java
+++ app/src/main/java/com/andrewfluck/blink/MainActivity.java
@@ -1,13 +1,26 @@
 package com.andrewfluck.blink;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                this.getWindow().getDecorView().setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            }
+        }
+
         setContentView(R.layout.activity_main);
     }
 }
